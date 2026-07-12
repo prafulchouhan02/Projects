38ad44b414b88da54b3394c4c2b630005e4e69f7
prafulchouhan02 <prafulchouhan02@gmail.com>
2026-07-12 09:08:26 +0530

    password generator

---
+++ password_generator.py
@@ -0,0 +1,22 @@
+import random
+import string
+if __name__ == "__main__":
+    s1 = string.ascii_lowercase
+    #print(s1)
+    s2 = string.ascii_uppercase
+    #print(s2)
+    s3 = string.digits
+    #print(s3)
+    s4 = string.punctuation
+    #print(s4)
+    plen = int(input("Enter password lengh\n")) #Todo1: Handle Gibbersish
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    
+    random.shuffle(s)
+   
+    print("Your password is: ")
+    print("".join(s[0:plen]))

2750ac49b19a3fc39057f18713924e7bf230d7c5
prafulchouhan02 <prafulchouhan02@gmail.com>
2026-07-16 09:01:21 +0530

    Madlibs game

---
+++ Madlibs.py
@@ -0,0 +1,21 @@
+name = input("Enter your name: ")
+birth_date = input("Enter your birthdate: ")
+birth_place = input("Enter your birth place: ")
+hobby = input("Enter your hobby: ")
+high_school = input("Enter your high school name: ")
+friend = input("Enter your firend name: ")
+branch_name = input("Enter your Branch name: ")
+collage_name = input("Enter your collage name name: ")
+Univarsity = input("Enter your Univarsity name: ")
+age = int(input("Enter your age: "))
+
+
+
+print("Title My Autobiography \n")
+
+print(f"""Hi, I'm {name}. I was born on {birth_date} in {birth_place}.
+      I love {hobby}. and it's been my passion since I was kid.
+      I went to {high_school}, where I made friend like {friend}.
+      After high school, I Studied {branch_name} at {collage_name} from {Univarsity}. 
+      Now, I'm {age}, and I'm excited about the future.
+      That's my story, in short !""")
